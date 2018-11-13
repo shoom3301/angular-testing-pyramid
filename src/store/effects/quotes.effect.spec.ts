10285c809e145e9c3722c039cb761bab9a297ccd
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2018-11-13 10:55:11 +0300

    Unit test for QuotesEffect - onFetchOne$ and onCreate$

--- src/store/effects/quotes.effect.spec.ts
+++ src/store/effects/quotes.effect.spec.ts
@@ -2,7 +2,7 @@ import {QuotesEffect} from '@store/effects/quotes.effect';
 import {provideMockActions} from '@ngrx/effects/testing';
 import {TestBed} from '@angular/core/testing';
 import {ReplaySubject} from 'rxjs';
-import {QuotesFetchAll, QuotesFetchedAll} from '@store/actions/quotes.action';
+import {QuotesCreate, QuotesFetchAll, QuotesFetchedAll, QuotesFetchedOne, QuotesFetchOne} from '@store/actions/quotes.action';
 import {QuotesService} from '@services/quotes.service';
 import {QuotesMockService} from '@services/quotesMock.service';
 import {quotesMock} from '@mocks/qoutes.mock';
@@ -37,5 +37,42 @@ describe('QuotesEffect - сайдэффекты на события цитат',
 
     expect(getQuotesListSpy).toHaveBeenCalledTimes(1);
     expect(expectedResult).toEqual(new QuotesFetchedAll(quotesMock));
+  });
+
+  it('При событии QuotesFetchOne запрашивается цитата и создается событие QuotesFetchedOne', () => {
+    const id = 1;
+    const getQuoteByIdSpy = spyOn(quotesService, 'getQuoteById').and.callThrough();
+    let expectedResult: QuotesFetchedOne;
+
+    actions.next(new QuotesFetchOne(id));
+
+    effects.onFetchOne$.subscribe(result => {
+      expectedResult = result;
+    });
+
+    expect(getQuoteByIdSpy).toHaveBeenCalledTimes(1);
+    expect(getQuoteByIdSpy).toHaveBeenCalledWith(id);
+    expect(expectedResult).toEqual(new QuotesFetchedOne(quotesMock.find(quote => quote.id === id)));
+  });
+
+  it('При событии QuotesCreate цитата отправляется на сервер и создается событие QuotesFetchedOne', () => {
+    const text = 'Test me please';
+    const author = 'Me';
+    const createSpy = spyOn(quotesService, 'create').and.callThrough();
+    let expectedResult: QuotesFetchedOne;
+
+    actions.next(new QuotesCreate(text, author));
+
+    effects.onCreate$.subscribe(result => {
+      expectedResult = result;
+    });
+
+    expect(createSpy).toHaveBeenCalledTimes(1);
+    expect(createSpy).toHaveBeenCalledWith(text, author);
+    expect(expectedResult).toEqual(new QuotesFetchedOne({
+      id: 4, // TODO get last id from quotesMock
+      text,
+      author
+    }));
   });
 });
