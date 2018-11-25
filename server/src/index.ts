25eb26bdd252fb65f27313dd1fc82bf7c060918d
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2018-11-25 16:03:03 +0300

    Server refactoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,5 @@
-import {createServer, IncomingMessage, ServerResponse} from 'http';
-import {routes} from './routes';
+import {PORT} from './config';
+import {quotesRoutes} from './routes/quotes.routes';
+import {runServer} from './utils/server.utils';
 
-const port = 8041;
-
-const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  const [path] = (req.url || '').split('?');
-  const routeName = `${req.method} ${path}`;
-  const route = routes[routeName];
-
-  if (route) {
-    return route(req, res);
-  }
-
-  res.statusCode = 404;
-  res.end();
-});
-
-server.listen(port);
-
-console.log(`Server started. http://localhost:${port}`);
+runServer(PORT, quotesRoutes);
---
+++ server/src/routes/quotes.routes.ts
@@ -0,0 +1,54 @@
+import {IncomingMessage, ServerResponse} from 'http';
+import {
+  createNewQuote,
+  findQuoteById,
+  getRequestQuoteId,
+  loadQuotes,
+  saveQuotes,
+  validateQuoteCreateRequest,
+  validateQuoteId
+} from '../utils/quotes.utils';
+import {createRequestResolver, getRequestJson} from '../utils/server.utils';
+import {IQuote} from '../models/IQuote';
+import {BadRequestError} from '../models/BadRequestError';
+import {IQuoteCreateRequest} from '../models/IQuoteCreateRequest';
+import {Routes} from '../models/Routes';
+
+async function quotesGetList() {
+  return loadQuotes();
+}
+
+async function quotesGetOne(req: IncomingMessage) {
+  const id = getRequestQuoteId(req);
+  const quoteIdIsValid = validateQuoteId(id);
+
+  if (!quoteIdIsValid) {
+    throw new BadRequestError('Quote id is not valid!');
+  }
+
+  const quotes = loadQuotes();
+
+  return findQuoteById(quotes, id);
+}
+
+async function quotesCreate(req: IncomingMessage) {
+  const quoteCreateRequest = await getRequestJson<IQuoteCreateRequest>(req);
+  const createQuoteRequestIsValid = validateQuoteCreateRequest(quoteCreateRequest);
+
+  if (!createQuoteRequestIsValid) {
+    throw new BadRequestError('Quote create request is not valid!');
+  }
+
+  const quotes = loadQuotes();
+  const quote = createNewQuote(quoteCreateRequest, quotes);
+
+  saveQuotes([...quotes, quote]);
+
+  return quote;
+}
+
+export const quotesRoutes: Routes = {
+  'GET /quotes': createRequestResolver<IQuote[]>(quotesGetList, 'Get quotes list'),
+  'GET /quote': createRequestResolver<IQuote | null>(quotesGetOne, 'Get one quote'),
+  'POST /quote': createRequestResolver<IQuote>(quotesCreate, 'Create quote')
+};
